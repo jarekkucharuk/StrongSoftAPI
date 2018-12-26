655ceb5789986d7f10923ba54e99c264ad2556e6
jarekkucharuk <jarekkucharuk@gmail.com>
2018-12-26 11:03:15 +0100

    initial commit

---
+++ src/main/java/pl/StrongSoft/StrongSoftApplication.java
@@ -0,0 +1,14 @@
+package pl.StrongSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrongSoftApplication.class, args);
+	}
+
+}
+
