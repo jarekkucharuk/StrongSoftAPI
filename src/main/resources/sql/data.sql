72f975df6b929fca755b37b72bc300e9ba0e78e6
jarekkucharuk <jarekkucharuk@gmail.com>
2019-01-06 17:59:22 +0100

    Pracownik + PracownikAdres + dane do tych tabel

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO pracownik_adres VALUES (default, 'Polna', '22','11', '20-000','Sanok','Polska');
+INSERT INTO pracownik_adres VALUES (default, 'Pijana', '33','111', '20-222','Kielce','Polska');
+INSERT INTO pracownik_adres VALUES (default, 'Polna', '55','1111', '20-333','Płock','Polska');
+
+INSERT INTO pracownik VALUES (default, 'Adam', 'Wilk','adas@op.pl' , 1);
+INSERT INTO pracownik VALUES (default, 'Kuba', 'Olejek','kuba@op.pl' , 2);
+INSERT INTO pracownik VALUES (default, 'Ola', 'Wolna','ola@op.pl' , 3);
